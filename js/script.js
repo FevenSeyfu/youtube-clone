93473c553528ba827227202232f46da8011a1e4a
FevenSeyfu <fevenseyfu00@gmail.com>
2022-11-20 12:12:34 +0300

    complete basic layout with some cleaning up left to do

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// var player;
+// function onYouTubeIframeAPIReady() {
+//   player = new YT.Player('player', {
+//     height: '390',
+//     width: '640',
+//     videoId: 'M7lc1UVf-VE',
+//     playerVars: {
+//       'playsinline': 1
+//     },
+//     events: {
+//       'onReady': onPlayerReady,
+//       'onStateChange': onPlayerStateChange
+//     }
+//   });
+// }
